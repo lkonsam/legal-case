fd2f1f333c230aa1bcdd78137bcd8686700754de
Malemngalba <lkonsam@gmail.com>
2025-07-15 16:57:56 +0530

    head title change

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="bg-[url('./assets/Rectangle.png')] bg-cover bg-center bg-no-repeat  min-h-screen lg:p-20">
       <div className="flex flex-col md:flex-row md:align-bottom  items-stretch justify-center gap-5 lg:gap-20 p-4 lg:p-8 ">
-        <div className="flex flex-col-reverse md:flex-col items-center justify-between gap-5 md:gap-2">
+        <div className="w-full flex flex-col-reverse md:flex-col items-center justify-between gap-5 md:gap-2">
           <Banner />
           <Topic />
         </div>
